c0de6efcd7d3ca9e8ad527b20176631fca936958
ALPHISTUBE <65537475+ALPHISTUBE@users.noreply.github.com>
2023-10-28 20:59:41 +0600

    Added Spwaning Level Item
    >Adjust camera angle
    >Added System to Spawn Obstacle, box and flags.

--- Assets/Scripts/GridBuilder.cs
+++ Assets/Scripts/GridBuilder.cs
@@ -4,14 +4,43 @@ using UnityEngine;
 
 public class GridBuilder : MonoBehaviour
 {
+    //Settings
+    [Header("Settings")]
     [Range(3, 10)]
     public int gridSize = 3;
+    [Range(1, 6)]
+    public int maxFlags;
+    [Range(0, 0.5f)]
+    public float maxObstaclePercentage = 0.2f;
+
+    //Prefabs
+    [Header("Prefabs")]
+    public Transform obstacle;
+    public Transform box;
+    public Transform flag;
 
     //Caching
-    public Point[] gridPoints; 
+    private Point[] gridPoints;
+    private Transform[] boxes;
+    private int maxObstacle;
+    private Transform[] obstacles;
+    private Transform[] flags;
 
-    void Update()
+    //Parents
+    [Header("Parents")]
+    public Transform obstacleParent;
+    public Transform boxParent;
+    public Transform flagParent;
+
+    void Start()
     {
+        //Setting up Level Properties
+        boxes = new Transform[maxFlags];
+        flags = new Transform[maxFlags];
+        maxObstacle = (int)(gridSize * gridSize * Random.Range(0.3f, maxObstaclePercentage));
+        obstacles = new Transform[maxObstacle];
+
+
         //Creating Grid Points
         gridPoints = new Point[gridSize * gridSize];
         float offsetPosition = ((float)gridSize - 1) / 2;
@@ -22,6 +51,48 @@ public class GridBuilder : MonoBehaviour
                 gridPoints[i * gridSize + j].position = new Vector2(i - offsetPosition, j - offsetPosition);
             }
         }
+
+        //Spawing level Items
+        for (int i = 0; i < maxObstacle; i++) //Obstacle
+        {
+            obstacles[i] = Instantiate(obstacle, obstacleParent);
+            bool objPlacedDone = false;
+            while (!objPlacedDone)
+            {
+                int p = Random.Range(0, gridPoints.Length - 1);
+                if (gridPoints[p].obj == null)
+                {
+                    obstacles[i].position = new Vector3(gridPoints[p].position.x, 0, gridPoints[p].position.y);
+                    gridPoints[p].obj = obstacles[i];
+                    objPlacedDone = true;
+                }
+            }            
+        }
+
+        for (int i = 0; i < maxFlags; i++) //Obstacle
+        {
+            boxes[i] = Instantiate(box, boxParent);
+            boxes[i].GetComponent<Renderer>().material.color = Color.blue;
+            flags[i] = Instantiate(flag, flagParent);
+            flags[i].GetComponent<Renderer>().material.color = Color.green;
+            bool objPlacedDone = false;
+            while (!objPlacedDone)
+            {
+                int p = Random.Range(0, gridPoints.Length - 1);
+                if (gridPoints[p].obj == null)
+                {
+                    boxes[i].position = new Vector3(gridPoints[p].position.x, 0, gridPoints[p].position.y);
+                    flags[i].position = new Vector3(gridPoints[p].position.x, 0, gridPoints[p].position.y);
+                    gridPoints[p].obj = boxes[i];
+                    objPlacedDone = true;
+                }
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R)) UnityEngine.SceneManagement.SceneManager.LoadScene(0);
     }
 
     //Point Container
