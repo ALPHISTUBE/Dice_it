01103506978326bae2f8a7f59234d40182c55530
ALPHISTUBE <65537475+ALPHISTUBE@users.noreply.github.com>
2023-10-29 13:44:59 +0600

    BUG NeighbourCell
    IndexOutofBound

--- Assets/Scripts/GridBuilder.cs
+++ Assets/Scripts/GridBuilder.cs
@@ -20,7 +20,8 @@ public class GridBuilder : MonoBehaviour
     public Transform flag;
 
     //Caching
-    private Point[] gridPoints;
+    public Point[] gridPoints;
+    private float offsetPosition;
     private Transform[] boxes;
     private int maxObstacle;
     private Transform[] obstacles;
@@ -34,6 +35,17 @@ public class GridBuilder : MonoBehaviour
 
     void Start()
     {
+        SetLevel();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R)) UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
+
+    //Set Level Object
+    public void SetLevel()
+    {
         //Setting up Level Properties
         boxes = new Transform[maxFlags];
         flags = new Transform[maxFlags];
@@ -43,7 +55,7 @@ public class GridBuilder : MonoBehaviour
 
         //Creating Grid Points
         gridPoints = new Point[gridSize * gridSize];
-        float offsetPosition = ((float)gridSize - 1) / 2;
+        offsetPosition = ((float)gridSize - 1) / 2;
         for (int i = 0; i < gridSize; i++)
         {
             for (int j = 0; j < gridSize; j++)
@@ -52,8 +64,9 @@ public class GridBuilder : MonoBehaviour
             }
         }
 
-        //Spawing level Items
-        for (int i = 0; i < maxObstacle; i++) //Obstacle
+        /*
+        //Spawing Obstacle Items
+        for (int i = 0; i < maxObstacle; i++)
         {
             obstacles[i] = Instantiate(obstacle, obstacleParent);
             bool objPlacedDone = false;
@@ -66,10 +79,14 @@ public class GridBuilder : MonoBehaviour
                     gridPoints[p].obj = obstacles[i];
                     objPlacedDone = true;
                 }
-            }            
-        }
+            }
+        }*/
 
-        for (int i = 0; i < maxFlags; i++) //Obstacle
+        StartCoroutine(CheckNeighbourGridCell());
+
+        /*
+        //Spawing Box and Flags Items
+        for (int i = 0; i < maxFlags; i++)
         {
             boxes[i] = Instantiate(box, boxParent);
             boxes[i].GetComponent<Renderer>().material.color = Color.blue;
@@ -82,24 +99,74 @@ public class GridBuilder : MonoBehaviour
                 if (gridPoints[p].obj == null)
                 {
                     boxes[i].position = new Vector3(gridPoints[p].position.x, 0, gridPoints[p].position.y);
-                    flags[i].position = new Vector3(gridPoints[p].position.x, 0, gridPoints[p].position.y);
+                    flags[i].position = new Vector3(gridPoints[p].position.x, -0.3f, gridPoints[p].position.y);
                     gridPoints[p].obj = boxes[i];
                     objPlacedDone = true;
                 }
             }
-        }
-    }
-
-    private void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.R)) UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+        }*/
     }
 
     //Point Container
+    [System.Serializable]
     public struct Point
     {
         public Vector2 position;
         public Transform obj;
+    }
+
+    IEnumerator CheckNeighbourGridCell()
+    {
+        for (int i = 0; i < gridPoints.Length; i++)
+        {
+            print(i);
+            yield return new WaitForSeconds(0.2f);
+            Point pC = gridPoints[i]; //center point
+            Point pR; //Right point
+            Point pL; //Left point
+            Point pU; //Up point
+            Point pD; //Down point
+
+            Transform o = Instantiate(box, new Vector3(pC.position.x, 0, pC.position.y), Quaternion.identity, boxParent);
+            o.GetComponent<Renderer>().material.color = Color.green;
+            Transform o1 = o;
+            Transform o2 = o;
+            Transform o3 = o;
+            Transform o4 = o;
+            //Y for left-right and X for up-down
+            if (pC.position.y <= offsetPosition)
+            {
+                pR = gridPoints[i + 1];
+                o1 = Instantiate(box, new Vector3(pR.position.x, 0, pR.position.y), Quaternion.identity, boxParent);
+                o1.GetComponent<Renderer>().material.color = Color.red;
+            }
+            if (pC.position.y >= -offsetPosition)
+            {
+                pL = gridPoints[i - 1];
+                o2 = Instantiate(box, new Vector3(pL.position.x, 0, pL.position.y), Quaternion.identity, boxParent);
+                o2.GetComponent<Renderer>().material.color = Color.red;
+            }
+            if (pC.position.x >= -offsetPosition)
+            {
+                pU = gridPoints[i - gridSize];
+                o3 = Instantiate(box, new Vector3(pU.position.x, 0, pU.position.y), Quaternion.identity, boxParent);
+                o3.GetComponent<Renderer>().material.color = Color.red;
+            }
+            if (pC.position.x <= offsetPosition)
+            {
+                pD = gridPoints[i + gridSize];
+                o4 = Instantiate(box, new Vector3(pD.position.x, 0, pD.position.y), Quaternion.identity, boxParent);
+                o4.GetComponent<Renderer>().material.color = Color.red;
+            }
+
+            yield return new WaitForSeconds(0.5f);
+
+            Destroy(o);
+            Destroy(o1);
+            Destroy(o2);
+            Destroy(o3);
+            Destroy(o4);
+        }
     }
 
     private void OnDrawGizmosSelected()
